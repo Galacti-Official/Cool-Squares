9f2bd1f986623185e3b18b5b70767faf51d83b45
janzezno <jan.masa889@gmail.com>
2026-03-06 16:29:41 +0100

    dadadasdca
    dada

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
-import StatsBar from "@/components/StatsBar";
 import Features from "@/components/Features";
 import HowItWorks from "@/components/HowItWorks";
 import CtaBanner from "@/components/CtaBanner";
